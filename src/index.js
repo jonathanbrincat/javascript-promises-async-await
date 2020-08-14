9b0385ed1b8fe015784b15048e0dbdd81cd978af
Jonathan Brincat <jonathan@pix8.co.uk>
2020-08-15 00:43:56 +0100

    test were failing because i removed the now redundant json import; dead code and completely unrelated to what was being assessed; feels fragile and flakey

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import { fetchWithTimeout, fetchMovies, fetchBooks } from "./services";
+const movies = require("./data/movies.json");
 
 function getBooksAndMovies() {
   return Promise.all([fetchBooks(), fetchMovies()])
