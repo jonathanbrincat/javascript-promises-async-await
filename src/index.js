b9197e8496991fc06fd3fdc21091d29e33c81406
Jonathan Brincat <jonathan@pix8.co.uk>
2020-08-15 00:17:27 +0100

    task 3 completed; tests are fussy and opinionated

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-export function fetchMovies() {}
+import { fetchWithTimeout } from "./services";
+
+// import * as movies from "./data/movies.json";
+const movies = require("./data/movies.json");
+
+export function fetchMovies() {
+  const resolveFunction = () => movies;
+
+  return fetchWithTimeout(1000).then(resolveFunction);
+}
+
+const moviePromise = fetchMovies();
+moviePromise.then((results) => {
+  console.log(results);
+});
